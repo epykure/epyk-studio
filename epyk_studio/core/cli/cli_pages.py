5069907d6228cb56d7eadf4a36c90b6dbe3db321
marlyk <smith.pyotr@gmail.com>
2020-08-18 19:14:26 +0100

    Fix packaging

--- epyk_studio/core/cli/cli_pages.py
+++ epyk_studio/core/cli/cli_pages.py
@@ -4,6 +4,8 @@ import sys
 import argparse
 import inspect
 
+from epyk.core.cli import utils
+
 
 def __write_page(path, name, template):
   """
@@ -360,6 +362,67 @@ page.headers.dev()
 ''')
 
 
+def transpile_parser(subparser):
+  """
+  Description:
+  ------------
+  Paser for the transpile CLI
+
+  Attributes:
+  ----------
+  :param subparser: subparser
+  """
+  subparser.set_defaults(func=transpile)
+  subparser.add_argument('-n', '--name', help='''The name of the page to be transpiled (without the extension)''')
+  subparser.add_argument('-p', '--path', help='''The path where the new environment will be created: -p /foo/bar''')
+
+
+def transpile(args):
+  """
+  Description:
+  ------------
+  Transpile a specific report
+
+  Attributes:
+  ----------
+  :param parser: -p, The path where the new environment will be created: -p /foo/bar
+  :param parser: -n, The name of the page to be transpiled: -n home
+  """
+  project_path = args.path or os.getcwd()
+  sys.path.append(project_path)
+  report_path = utils.get_report_path(project_path, raise_error=False)
+  sys.path.append(report_path)
+  ui_setting_path = os.path.join(report_path, '..', 'ui_settings.py')
+  install_modules, split_files, view_folder, settings = False, False, 'views', None
+  if os.path.exists(ui_setting_path):
+    settings = __import__('ui_settings')
+    install_modules = settings.INSTALL_MODULES
+    split_files = settings.SPLIT_FILES
+    view_folder = settings.VIEWS_FOLDER
+  views = []
+  if args.name is None:
+    for v in os.listdir(report_path):
+      if v.endswith(".py"):
+        views.append(v[:-3])
+  else:
+    views = [args.name]
+  for f in views:
+    try:
+      print("Transpilling files: %s" % f)
+      mod = __import__(f, fromlist=['object'])
+      page = utils.get_page(mod)
+      if settings is not None:
+        page.node_modules(settings.PACKAGE_PATH, alias=settings.SERVER_PACKAGE_URL)
+      if not os.path.exists(view_folder):
+        # If it is not an absolute path
+        view_folder = os.path.join(report_path, '..', '..', view_folder)
+      output = page.outs.html_file(path=view_folder, name=f, split_files=split_files, install_modules=install_modules,
+                                   options={"css_route": '/css', "js_route": '/js'})
+      print(output)
+    except Exception as err:
+      print(err)
+
+
 def main():
   """
   Description:
@@ -368,6 +431,7 @@ def main():
   """
   parser_map = {
     'new': (new_parsers, '''Create new environment'''),
+    'transpile': (transpile_parser, '''Transpile a script to web objects'''),
     'dashboard': (new_template, '''Create new dashboard'''),
     'management': (new_template, '''Create new Management report'''),
     'shop': (new_template, '''Create new shop'''),
@@ -385,7 +449,7 @@ def main():
   subparser.required = True
   for func, parser_init in parser_map.items():
     new_parser = subparser.add_parser(func, help=parser_init[1])
-    if func != 'new':
+    if func not in ['new', 'transpile']:
       new_parser.set_defaults(func=globals()[func])
     parser_init[0](new_parser)
   args = arg_parser.parse_args(sys.argv[1:])
--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ def install_required():
 setuptools.setup(
     name="epyk_studio",
     author="epykure",
-    version="0.0.8",
+    version="0.0.9",
     author_email="smith.pyotr@gmail.com",
     description="A simple way to create rich interactive websites from Python",
     long_description=long_description,
