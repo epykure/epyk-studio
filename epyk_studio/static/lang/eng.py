e1d78e66e57b24b9b7ea2d9cc97bedac641b19d4
poolppy <55318816+poolppy@users.noreply.github.com>
2020-10-03 14:54:15 +0100

    Update eng.py

--- epyk_studio/static/lang/eng.py
+++ epyk_studio/static/lang/eng.py
@@ -5,7 +5,7 @@
 TEXT_1 = "Welcome to Epyk"
 TEXT_2 = "The FullStack Python Framework"
 TEXT_3 = '''
-The Python collaborative framework to link the two most popular ecosystem Pytohn and JavaScript.
-Coding is much simpler since this will fully leverage on the Python syntax to manage your web designs
+The Python collaborative framework to link the two most popular ecosystems Python and JavaScript.
+Coding is much simpler since this will fully leverage on the Python syntax to manage your web designs.
 '''
 TEXT_4 = 'Get Started'
